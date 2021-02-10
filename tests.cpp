07b839a457f303be57eff907c7890527935da699
NikitaMushchak <39431136+NikitaMushchak@users.noreply.github.com>
2021-02-10 21:41:16 +0300

    reverse sum seems ok

--- tests.cpp
+++ tests.cpp
@@ -115,6 +115,7 @@ struct ListNode {
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode* next) : val(x), next(next) {}
+    ~ListNode() {}
     void Push_Back(ListNode *&plist,int number) {
         
         if (plist == nullptr) {
@@ -168,7 +169,7 @@ int main(int argc, char **argv) {
     }
    
     ListNode** itlist = &list1;
-    list1->Print_List(itlist);
+    //list1->Print_List(itlist);
    
 
     ListNode list(0);
@@ -178,10 +179,79 @@ int main(int argc, char **argv) {
     for (int i = 0; i < 10;i++) {
         list.Push_Back(pList, i);
     }
-    list.Print_List(list);
+    //list.Print_List(list);
    
-    
+    ListNode p1(0), p2(0);
 
+    ListNode* t1 = &p1;
+    ListNode* t2 = &p2;
+
+    /*for (int i = 0; i < 6; ++i) {
+        p1.Push_Back(t1, i+1);
+        p2.Push_Back(t2, i+1);
+    }*/
+
+    /*p1.Push_Back(t1, 2);
+    p1.Push_Back(t1, 4);
+    p1.Push_Back(t1, 3);
+
+    p2.Push_Back(t2, 5);
+    p2.Push_Back(t2, 6);
+    p2.Push_Back(t2, 4);*/
+
+   /* p1.Push_Back(t1, 0);
+    p2.Push_Back(t2, 0);*/
+
+    p1.Push_Back(t1, 9);
+    p1.Push_Back(t1, 9);
+    p1.Push_Back(t1, 9);
+    p1.Push_Back(t1, 9);
+    p1.Push_Back(t1, 9);
+    p1.Push_Back(t1, 9);
+    p1.Push_Back(t1, 9);
+
+    p2.Push_Back(t2, 9);
+    p2.Push_Back(t2, 9);
+    p2.Push_Back(t2, 9);
+    p2.Push_Back(t2, 9);
+
+    t1 = (&p1)->next;
+    t2 = (&p2)->next;
+    int nextval = 0;
+
+    ListNode p3(0);
+    ListNode* t3 = &p3;
+    int firstval = 0;
+    int secondval = 0;
+    int result = 0;
+    int s = 0;
+    int it = 0;
+    while (t1 || t2 || s) {
+        if (t1) {
+            firstval = t1->val;
+            t1 = t1->next;
+        }
+        else {
+            firstval = 0;
+        }
+        if (t2) {
+            secondval = t2->val;
+            t2 = t2->next;
+        }
+        else {
+            secondval = 0;
+        }
+        result = firstval + secondval + s;
+        nextval = result % 10;
+
+        p3.Push_Back(t3, nextval);
+
+        s = result / 10;
+         
+
+    }
+
+    p3.Print_List(p3);
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
